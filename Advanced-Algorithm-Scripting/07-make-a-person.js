042088105eb112d924ee7f5f472fe8fa70f535df
taosang1992 <taosang1992@gmail.com>
2017-12-09 07:15:58 +0800

    高级算法练习

--- Advanced-Algorithm-Scripting/07-make-a-person.js
+++ Advanced-Algorithm-Scripting/07-make-a-person.js
@@ -1,6 +1,31 @@
 var Person = function(firstAndLast) {
-  return firstAndLast;
+  var nameArray = firstAndLast.split(' ');
+  var first = nameArray[0];
+  var last = nameArray[1];
+  var fullName = first + last;
+  this.getFirstName = function () {
+    return first;
+  };
+  this.getLastName = function () {
+    return last;
+  };
+  this.getFullName = function () {
+    return first + ' ' + last;
+  };
+  this.setFirstName = function (firstName) {
+    first = firstName;
+  };
+  this.setLastName = function (lastName) {
+    last = lastName;
+  };
+  this.setFullName = function (fullName) {
+    var fullNameArr = fullName.split(' ');
+    first = fullNameArr[0];
+    last = fullNameArr[1];
+    fullName = first + ' ' + last;
+  };
 };
 
 var bob = new Person('Bob Ross');
-bob.getFullName();
+console.log(bob.getFullName());
+console.log(Object.keys(bob).length);
--- Advanced-Algorithm-Scripting/08-map-the-debris.js
+++ Advanced-Algorithm-Scripting/08-map-the-debris.js
@@ -1,7 +1,17 @@
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
-  return arr;
+  var result = [];
+  arr.forEach(function (item) {
+    var R = earthRadius + item.avgAlt;
+    var T = Math.round(2*Math.PI*Math.sqrt(Math.pow(R,3)/GM));
+    result.push({
+      name: item.name,
+      orbitalPeriod: T
+    })
+  });
+  console.log(result);
+  return result;
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
